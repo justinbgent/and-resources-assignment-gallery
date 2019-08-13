3ecfa8ba967a2f6f6a843982238fefec8fcca7ae
Justin Gent <justinbgent@gmail.com>
2019-08-12 19:12:08 -0600

    App is finished and working.

--- GalleryApp/app/src/main/java/com/example/galleryapp/MainActivity.kt
+++ GalleryApp/app/src/main/java/com/example/galleryapp/MainActivity.kt
@@ -14,7 +14,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val imageViews: ArrayList<ImageView> = ArrayList(10)
+        var imageView0 = ImageView(this)
+        var imageView1 = ImageView(this)
+        var imageView2 = ImageView(this)
+        var imageView3 = ImageView(this)
+        var imageView4 = ImageView(this)
+        var imageView5 = ImageView(this)
+        var imageView6 = ImageView(this)
+        var imageView7 = ImageView(this)
+        var imageView8 = ImageView(this)
+        var imageView9 = ImageView(this)
+
+        var imageViews: Array<ImageView> = arrayOf(
+            imageView0, imageView1, imageView2, imageView3, imageView4, imageView5, imageView6,
+            imageView7, imageView8, imageView9
+        )
+
         imageViews[0].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.adventure_automobile_classic_2533092))
         imageViews[1].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.afterglow_beach_cliff_2555285))
         imageViews[2].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.alley_architecture_buildings_2526517))
@@ -25,6 +40,17 @@ class MainActivity : AppCompatActivity() {
         imageViews[7].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.clouds_coconut_trees_daylight_2486168))
         imageViews[8].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.colorful_colourful_houses_2501965))
         imageViews[9].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.wallpaper_astronomy_astrophotography_2538107))
+
+        imageViews[0].contentDescription = getString(R.string.automobile)
+        imageViews[1].contentDescription = getString(R.string.smooth_cliff)
+        imageViews[2].contentDescription = getString(R.string.alley)
+        imageViews[3].contentDescription = getString(R.string.vegas_night)
+        imageViews[4].contentDescription = getString(R.string.fireworks)
+        imageViews[5].contentDescription = getString(R.string.rose)
+        imageViews[6].contentDescription = getString(R.string.shoes)
+        imageViews[7].contentDescription = getString(R.string.palm_leaves)
+        imageViews[8].contentDescription = getString(R.string.roof_tops)
+        imageViews[9].contentDescription = getString(R.string.outer_space)
 
 //        <style name="ImageStyle">
 //        <item name="android:padding">10dp</item>
@@ -41,9 +67,9 @@ class MainActivity : AppCompatActivity() {
             it.layoutParams = layoutParams
             it.scaleType = ImageView.ScaleType.FIT_CENTER
 
-            if (i++ < imageViews.size / 2){
+            if (i++ < imageViews.size / 2) {
                 left_column.addView(it)
-            }else{
+            } else {
                 right_column.addView(it)
             }
         }
